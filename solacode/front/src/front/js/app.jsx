826a77374057ada335b3b70196e7d3c7315b8f5e
solacode <solacode.ahw@gmail.com>
2024-08-07 19:31:11 +0330

    context api, header, footer setup

--- solacode/front/src/front/js/app.jsx
+++ solacode/front/src/front/js/app.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Outlet } from "react-router-dom";
+
+import Footer from "./components/footer";
+
+import { SettingsContext, settingsReducer } from "./utils/context";
+
 
 function App() {
 
+  const [settings, updateSettings] = useReducer(settingsReducer,{
+    theme: 'light',
+    lang: 'fa',
+  });
+
   return (
-    <Outlet />
+    <SettingsContext.Provider value={settings}><>
+      <header>
+        <img src="/assets/front/img/logo.png" />
+      </header>
+      <Outlet />
+      <Footer onChangeSettings={updateSettings} />
+    </></SettingsContext.Provider>
   );
 }
 
