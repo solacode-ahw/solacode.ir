3ff74e19e79a54ed68f1bc8fbc8fd4a3d61d6d49
solacode <solacode.ahw@gmail.com>
2024-08-07 12:01:13 +0330

    post id added

--- solacode/front/src/front/js/post.jsx
+++ solacode/front/src/front/js/post.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function Post() {
 
+  const { id } = useParams();
+
   return (
-    <h2>Post page</h2>
+    <h2>Post page: {id}</h2>
   );
 }
 
