1d92f45acf741c9b2950588f2407854448e6458b
solacode <solacode.ahw@gmail.com>
2024-08-12 02:13:02 +0330

    icon font setup

---
+++ solacode/front/src/js/utils/icons.js
@@ -0,0 +1,16 @@
+
+const icons = {
+    bell: '\\e900',
+    box: '\\e901',
+    checkbox: '\\e902',
+    dark: '\\e908',
+    down: '\\e903',
+    download: '\\e904',
+    instagram: '\\e905',
+    left: '\\e90c',
+    light: '\\e90b',
+    mail: '\\e906',
+    right: '\\e909',
+    search: '\\e907',
+    share: '\\e90a',
+};
