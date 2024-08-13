d4f629f8c03dbbe0cb9dc015f15f91ae61758189
solacode <solacode.ahw@gmail.com>
2024-08-14 00:42:04 +0330

    footer complete

--- solacode/front/src/js/components/footer.jsx
+++ solacode/front/src/js/components/footer.jsx
@@ -1,26 +1,53 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import { SettingsContext } from "../utils/context";
+import { footer } from "../utils/translations";
+
+import '../../css/footer.sass';
 
 
 function Footer({onChangeSettings}){
     const blog = useLocation().pathname.includes('/blog');
     const settings = useContext(SettingsContext);
 
-    if(blog){
-        return (
-            <footer>
-                <p>this is blog footer</p>
-            </footer>
-        );
-    } else {
-        return (
-            <footer>
-                <p>this is default footer</p>
-            </footer>
-        );
-    }
+    const [langOpen,setLangOpen] = useState(false);
+
+    const changeLang = (lang) => {
+        if(lang!==settings.lang){
+            onChangeSettings(lang);
+        }
+        setLangOpen(false);
+    };
+
+    return (
+        <footer>
+            <section id="information">
+                <p>{footer[settings.lang].font[0]} <a href="https://fontiran.com/fonts/borna/?ref=868">{footer[settings.lang].font[1]}</a></p>
+                <p>{footer[settings.lang].icon[0]} <a href="https://github.com/480-Design/Solar-Icon-Set">{footer[settings.lang].icon[1]}</a></p>
+            </section>
+            <section id="contact">
+                <a href="/api/insta" className="icon instagram"></a>
+                <a href="/api/mail" className="icon mail"></a>
+            </section>
+            <section id="control">
+                <button id="theme-switch" type="button" onClick={()=>onChangeSettings('theme')}>
+                    <span className={`icon ${settings.theme}`}></span>
+                </button>
+                {blog?null:
+                    <aside id="language-picker">
+                        <button type="button" onClick={()=>setLangOpen(true)}>
+                            <span>{settings.lang==='fa'?'فا':'en'}</span> <span id="arrow" className="icon down"></span>
+                        </button>
+                        <dialog open={langOpen}>
+                            <p onClick={()=>changeLang('fa')}>فا</p>
+                            <p onClick={()=>changeLang('en')}>en</p>
+                        </dialog>
+                    </aside>
+                }
+            </section>
+        </footer>
+    );
 }
 
 export default Footer;
---
+++ solacode/front/src/js/utils/translations.js
@@ -0,0 +1,11 @@
+
+export const footer = {
+    en: {
+        font: ['Font used in this website:','Borna Font'],
+        icon: ['Iconset used in this website:','Solar Icons'],
+    },
+    fa: {
+        font: ['فونت مورد استفاده در این وبسایت:','فونت برنا'],
+        icon: ['مجموعه‌ آیکون مورد استفاده در این وبسایت:','آیکون‌های Solar'],
+    },
+};
