f01c3c2d4c79f41e8951af3c15fff2c4e1b2eb79
solacode <solacode.ahw@gmail.com>
2024-08-26 10:21:05 +0330

    minor bug fix

--- solacode/api/permissions.py
+++ solacode/api/permissions.py
@@ -1,18 +1,18 @@
 from rest_framework import permissions
 
-class HirePermission(permissions.BasePermission):
+class RegisterPermission(permissions.BasePermission):
     def has_permission(self,request,view):
         if request.method=='POST':
             return True
         else:
-            return permissions.IsAdminUser.has_permission(request,view)
+            return permissions.IsAdminUser.has_permission(self,request,view)
         
 class ContentPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method=='GET':
             return True
         else:
-            return permissions.IsAdminUser.has_permission(request,view)
+            return permissions.IsAdminUser.has_permission(self,request,view)
         
 class AllowAny(permissions.AllowAny):
     pass
