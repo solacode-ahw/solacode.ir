6f3ef2ca31ba97cfd64c1c339acddb9cc64ef2f9
solacode <solacode.ahw@gmail.com>
2024-08-28 00:46:41 +0330

    post page implemented

--- solacode/front/src/js/pages/post.jsx
+++ solacode/front/src/js/pages/post.jsx
@@ -1,13 +1,103 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import { Converter, setFlavor } from "showdown";
+import { Parser } from "html-to-react";
+setFlavor('github');
+
+import { SolaButton } from "../components/basic";
+import Subscribe from "../components/subscribe";
+
+import '../../css/post.sass';
+
 
 function Post() {
-
   const { id } = useParams();
+  const [article,setArticle] = useState(null);
+  const subRef = useRef(null);
+  const msgRef = useRef(null);
 
-  return (
-    <h2>Post page: {id}</h2>
-  );
+  useEffect(()=>{
+    fetch(`/api/db/blog/${id}/`).then((response)=>{
+      if(response.ok){
+        return response.json();
+      } else {
+        throw new Error({
+          msg: 'something wrong with the request',
+          response: response
+        });
+      }
+    }).then((resultJson)=>{
+      setArticle(resultJson);
+    }).catch((error)=>{
+      console.log(error);
+    });
+  },[id]);
+
+  const openSubscribe = () => {
+    subRef.current.showModal();
+  };
+  const closeSubscribe = () => {
+    subRef.current.close();
+  };
+  const copyLink = () => {
+    navigator.clipboard.writeText(window.location.href).then((res)=>{
+      msgRef.current.showModal();
+    }).catch((error)=>{
+      console.log('fail:',error);
+    });
+  };
+  const closeMsg = () => {
+    msgRef.current.close();
+  };
+
+  if(article){
+    return (
+      <>
+        <Helmet>
+          <title>{article.title}</title>
+          <meta name="keywords" content={article.keywords} />
+          <meta name="description" content={article.summary} />
+          <meta name="author" content="SolaCode" />
+          <meta name="robots" content="index,follow" />
+        </Helmet>
+        <article id="blog-post">
+          {article.image?
+            <img src={article.image} id="blog-image" />
+          :null}
+          <h2>{article.title}</h2>
+          <section id="blog-info">
+            <p>{
+              new Intl.DateTimeFormat('fa-IR',{
+                month: 'long',
+                calendar: 'persian'
+              }).format(new Date(article.date)) + " " +
+              new Intl.DateTimeFormat('fa-IR',{
+                year: 'numeric',
+                calendar: 'persian'
+              }).format(new Date(article.date))
+            }</p>
+            <p>نویسنده: بهار پایدار</p>
+          </section>
+          <p>{article.summary}</p>
+          <section id="blog-body">{
+            Parser().parse(new Converter().makeHtml(article.body))
+          }</section>
+          <section id="action-wrapper">
+            <SolaButton icon="share" action={copyLink} />
+            <SolaButton icon="bell" action={openSubscribe} />
+          </section>
+        </article>
+        <dialog ref={subRef}>
+          <Subscribe close={closeSubscribe} />
+        </dialog>
+        <dialog ref={msgRef}>
+          <p>لینک این صفحه کپی شد!</p>
+          <SolaButton action={closeMsg} label="باشه" />
+        </dialog>
+      </>
+    );
+  }
 }
 
 export default Post;
